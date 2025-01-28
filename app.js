c0dd755b21992fe49bf4e02f4f23850d65bb00ad
Carolina-Zampieri <carolina.zampieri89@gmail.com>
2025-01-28 15:51:01 -0300

    atualizando função lista

--- app.js
+++ app.js
@@ -33,5 +33,9 @@ function limparInput() {
 
 function listaNomes() {
     let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = amigos.join('</br>');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
 }
